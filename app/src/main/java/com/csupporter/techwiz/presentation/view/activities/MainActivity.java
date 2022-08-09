7a51c57f1e9745ea88317cd9b796ad19446d71d8
NAMTRAN <nam.td.986@aptechlearning.edu.vn>
2022-08-09 21:53:08 +0700

    nam test commit part 2

--- app/src/main/java/com/csupporter/techwiz/presentation/view/activities/MainActivity.java
+++ app/src/main/java/com/csupporter/techwiz/presentation/view/activities/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//  dhgjagdajsgdjas
+//  alo alo aloooo
     }
 
     @Override
