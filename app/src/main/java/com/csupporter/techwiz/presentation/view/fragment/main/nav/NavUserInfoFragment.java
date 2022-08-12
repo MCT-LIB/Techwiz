bb160015ddc8b0a16d432a2de5f28d6009f7e01f
SON.NC <son15052002@gmail.com>
2022-08-12 11:08:45 +0700

    Merge

--- app/src/main/java/com/csupporter/techwiz/presentation/view/fragment/main/nav/NavUserInfoFragment.java
+++ app/src/main/java/com/csupporter/techwiz/presentation/view/fragment/main/nav/NavUserInfoFragment.java
@@ -35,53 +35,9 @@ public class NavUserInfoFragment extends BaseFragment implements View.OnClickLis
 
 
     private NavUserInfoPresenter navUserInfoPresenter;
-    private ConfirmDialog dialog;
-
-
-
     View view;
     TextView tvName, tvEmail;
     RelativeLayout itemLogout, itemHealthTrack;
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public NavUserInfoFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment NavUserInfoFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static NavUserInfoFragment newInstance(String param1, String param2) {
-        NavUserInfoFragment fragment = new NavUserInfoFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -114,24 +70,23 @@ public class NavUserInfoFragment extends BaseFragment implements View.OnClickLis
         switch (v.getId()) {
             case R.id.item_logout:
                 if (getActivity() != null) {
+                    new ConfirmDialog(getActivity(), R.drawable.ic_logout, getString(R.string.dialog_confirm_logout_msg),
+                            new ConfirmDialog.OnConfirmListener() {
+                                @Override
+                                public void onConfirm(BaseOverlayDialog overlayDialog) {
+                                    navUserInfoPresenter.logOut();
+                                    Intent intent = new Intent(getContext(), AuthenticateActivity.class);
+                                    startActivity(intent);
+                                    if (getActivity() != null) {
+                                        getActivity().finish();
+                                    }
+                                }
 
-                    dialog = new ConfirmDialog(getActivity(), new ConfirmDialog.ShowConfirmDialog() {
-                        @Override
-                        public void onConfirm() {
-                            navUserInfoPresenter.logOut();
-                            Intent intent = new Intent(getContext(), AuthenticateActivity.class);
-                            startActivity(intent);
-                            if (getActivity() != null) {
-                                getActivity().finish();
-                            }
-                        }
-
-                        @Override
-                        public void onCancel() {
-                            dialog.dismiss();
-                        }
-                    });
-                    dialog.create(null);
+                                @Override
+                                public void onCancel(BaseOverlayDialog overlayDialog) {
+                                    overlayDialog.dismiss();
+                                }
+                            }).create(null);
 
                 }
                 break;
