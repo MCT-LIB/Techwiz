70db473a1f389857aff95140db84f93833e0b434
NguyenLuan1010 <vanluan101002@gmail.com>
2022-08-15 08:22:15 +0700

    process prescription 2

--- app/src/main/java/com/csupporter/techwiz/domain/repository/ListPrescriptionDetailRepository.java
+++ app/src/main/java/com/csupporter/techwiz/domain/repository/ListPrescriptionDetailRepository.java
@@ -1,8 +1,16 @@
 package com.csupporter.techwiz.domain.repository;
 
+import androidx.annotation.Nullable;
+import androidx.core.util.Consumer;
+
 import com.csupporter.techwiz.domain.model.Account;
+import com.csupporter.techwiz.domain.model.HealthTracking;
 import com.csupporter.techwiz.domain.model.Prescription;
 
-public interface ListPrescriptionDetailRepository {
+import java.util.List;
 
+public interface ListPrescriptionDetailRepository {
+    void getAllPrescriptionDetail(Prescription prescription,
+                                  @Nullable Consumer<List<HealthTracking>> onSuccess,
+                                  @Nullable Consumer<Throwable> onError);
 }
